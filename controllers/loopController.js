51b70408de93c23243b0fbf306f66747261e6ca8
Mario Henmanuel <hemma.hvu@gmail.com>
2016-09-13 17:53:28 -0600

    correccion controlador

--- controllers/loopController.js
+++ controllers/loopController.js
@@ -2,7 +2,7 @@ function modal(id,content){
 	if ($("#"+id).length == 0){
 		$('body').append('<div id="'+id+'" class="modal" ></div>');
 		$.ajax({
-			url: '/'+content,async: true,
+			url: '/'+content,type: 'GET',async: true,
 			success: function(data) {
 				$('#'+id).append('<div id="modal"><span class="modalClose">×</span>'+data+'</div>');
 				var modal = document.getElementById(id);
