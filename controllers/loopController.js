f6601c4f84cf55989b91ea830deaeb49408ad57f
Mario Henmanuel <hemma.hvu@gmail.com>
2016-09-13 19:04:27 -0600

    Se agrega Faceook Login al controlador

--- controllers/loopController.js
+++ controllers/loopController.js
@@ -2,7 +2,7 @@ function modal(id,content){
 	if ($("#"+id).length == 0){
 		$('body').append('<div id="'+id+'" class="modal" ></div>');
 		$.ajax({
-			url: '/'+content,type: 'GET',async: true,
+			url:content,async:true,
 			success: function(data) {
 				$('#'+id).append('<div id="modal"><span class="modalClose">×</span>'+data+'</div>');
 				var modal = document.getElementById(id);
@@ -50,3 +50,47 @@ function plus(){//objeto longPress
     	} 
     });
 }
+function faceLogin(req){
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '1413878928878484',
+			xfbml      : true,
+			version    : 'v2.7'
+		});
+		statusUser();
+	};
+	function statusUser(){  
+		FB.getLoginStatus(function(response) {  
+			if (response.status === 'connected') {
+				plus();
+			} else if (response.status === 'not_authorized') {
+				modal('authModal','views/authorized.html');
+			} else {
+				$('#plusCont').append('<img id="plusLogIn" class="plus" src="style/img/fb.png">');
+				$('#plusLogIn').click(function(){
+					login();
+				});
+				localStorage.clear();
+			} 
+		});  
+	} 
+	function login(){  
+		FB.login(function(response){ 
+			if (response['status']  == "unknown"){
+				modal('authModal','views/authorized.html');
+			}else{
+				statusUser();
+			} 	  
+		},{scope: 'public_profile, user_likes, user_friends, email'});  
+	}
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_LA/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+}
+$(document).ready(function(){ 
+	faceLogin('acces');
+});
